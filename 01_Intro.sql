c8d73d23a10e1add6d6cdfe7843f7707d03a85db
Mkapoor29 <123291579+Mkapoor29@users.noreply.github.com>
2024-02-13 20:54:10 +0530

    Update 01_Intro.sql

--- 01_Intro.sql
+++ 01_Intro.sql
@@ -12,3 +12,7 @@
 
 -- To check structure of table 
     show coulmns from devices;
+
+
+-- To give default values in a column(for example, for a restraunt in ABC city majority of customers are from ABC city. hence deafult value of city can be set as ABC while creating the table.
+	create table customer(custId int not null , postCode varchar(10) default  "HA97DE", town VARCHAR(30) DEFAULT "Harrow"); 
