41ebb5b8de85efbaf57a7754f43b0ad7f5830b4b
Muskan Kapoor <123291579+Mkapoor29@users.noreply.github.com>
2026-02-02 19:46:15 -0500

    Delete 10_Length.sql

--- 10_Length.sql
+++
@@ -1,7 +0,0 @@
-select student_name, length(student_name) from students;
-
-select student_name, length(student_name) from students where student_name like "M%";
-
-SELECT name, capital FROM world WHERE length(name) = length(capital);
-
-
