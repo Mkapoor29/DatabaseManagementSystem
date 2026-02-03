ef4fbea78b4a65fc60e579d1c8d644fa67b52f06
Muskan Kapoor <123291579+Mkapoor29@users.noreply.github.com>
2026-02-03 00:32:34 -0500

    Update 06_SortingRows.sql

--- 06_SortingRows.sql
+++ 06_SortingRows.sql
@@ -10,3 +10,17 @@ SELECT * FROM students1 ORDER BY student_name, marks;
 
 -- selects all students, sorted ascending by the "name and descending by the marks column
 SELECT * FROM students1 ORDER BY student_name ASC, marks DESC;
+
+
+-- The expression subject IN ('chemistry','physics') can be used as a value - it will be 0 or 1.
+SELECT winner, subject, subject IN ('physics','chemistry')
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject,winner
+
+
+-- show the 1984 winners and subject ordered by subject and winner name; but list chemistry and physics last.
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject in ('physics','chemistry')
